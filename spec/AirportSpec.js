e51c0d3f3439f7550a10e1428ee4d0bb2bb6d27f
jebax <joel.eliot.baxter@gmail.com>
2018-10-30 12:29:04 +0000

    wrote and passed first test for airpot

--- spec/AirportSpec.js
+++ spec/AirportSpec.js
@@ -1,1 +1,15 @@
+describe("Airport", function() {
+  var Airport = require('../lib/Airport');
+  var airport;
 
+  beforeEach(function() {
+    airport = new Airport();
+  });
+
+  describe("An airport", function() {
+
+    it("can store planes", function() {
+      expect(airport.landedPlanes()).toEqual([])
+    });
+  });
+});
