0fd0918f634038bb8d185af274bccc6b863db04e
jebax <joel.eliot.baxter@gmail.com>
2018-10-31 11:57:42 +0000

    can check is airport full

--- spec/AirportSpec.js
+++ spec/AirportSpec.js
@@ -1,19 +1,29 @@
 describe("Airport", function() {
   var Airport = require('../lib/Airport');
   var airport;
+  var plane;
 
   beforeEach(function() {
     airport = new Airport();
+    fullAirport = new Airport();
+    var plane = jasmine.createSpy('plane')
   });
 
   describe("An airport", function() {
 
     it("can store planes", function() {
-      expect(airport.landedPlanes()).toEqual([])
+      expect(airport.getLandedPlanes()).toEqual([]);
     });
 
     it("has a default capacity of 20 planes", function() {
-      expect(airport.DEFAULT_CAPACITY).toEqual(20)
+      expect(airport.DEFAULT_CAPACITY).toEqual(20);
+    });
+
+    it('is full if number of landed planes equals capacity', function() {
+      for(i = 0; i < 20; i++) {
+        fullAirport._landedPlanes.push(plane)
+      }
+      expect(fullAirport).toBeFull;
     })
   });
 });
